d9c936a4d85306c37d853153ef3231b7c9103bc5
Brahian <brahian.verac@gmail.com>
2023-05-15 23:59:49 -0500

    movies project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # react
+/How to create a react app whitout next.js 
+ -- npm create-react-app ./  
+/start application 
+ -- npm start 
---
+++ projects/movies/src/MovieCard.js
@@ -0,0 +1,17 @@
+export default function MovieCard({movie}) {
+    return (
+        <div className="movie">
+            <div>
+                <p>{movie.Year}</p>
+            </div>
+            <div>
+                <img src={movie.Poster === "N/A" ? 'http://via.placeholder.com/400' : movie.Poster} alt={movie.Title}/>
+            </div>
+            <div>
+                <span>{movie.Type}</span>
+                <h3>{movie.Title}</h3>
+            </div>
+        </div>
+    )
+}
+
