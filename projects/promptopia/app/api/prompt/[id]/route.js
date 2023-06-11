b65c098781438ce4337b39235b16ccd92b22daf1
Brahian <brahian.verac@gmail.com>
2023-06-10 22:48:20 -0500

    endpoint appis

--- projects/promptopia/app/api/prompt/[id]/route.js
+++ projects/promptopia/app/api/prompt/[id]/route.js
@@ -14,4 +14,39 @@ export const GET = async(request,{params}) =>{
 }
 
 // PATCH (update)
+
+export const PATCH = async (request, {params})=>{
+    const  {prompt, tag} = await request.json();
+
+    try{
+        await connectToDB();
+        const existingPrompt = await Prompt.findById(params.id);
+        if(!existingPrompt) return new Response("Prompt not found", {status:404});
+
+        existingPrompt.prompt = prompt
+        existingPrompt.tag = tag;
+
+        await existingPrompt.save();
+
+        return new Response(JSON.stringify(existingPRompt),{status:200})
+
+    }catch(error){
+
+        return new Response("Failed to update prompt", {status:500})
+
+    }
+}
+
 // DELETE () Deleted
+export const DELETE = async(request, {params})=>{
+    try{
+        await connectToDB();
+
+        await Prompt.findByIdAndRomove(params.id)
+
+        return new Response("Prompt deleted successfully",{status:200});
+
+    }catch(error){
+        return new Response("Failes to delete prompt",{status:500})
+    }
+}
